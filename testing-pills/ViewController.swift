235164d232a212ca2aaa706726d4c52ce6e0fbed
ecoenghk <ecoenghk@gmail.com>
2017-04-19 16:22:28 +0800

    Initial Commit

---
+++ testing-pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing-pills
+//
+//  Created by Ecoenghk on 19/4/2017.
+//  Copyright © 2017年 Ecoenghk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
